032b83d0c134846271ed2b63e41dd8e3bef6dd9e
hafidznaufl <hafidznaufl@gmail.com>
2023-06-22 09:36:12 +0700

    Deploy React

--- src/pages/movies/NowPlaying.js
+++ src/pages/movies/NowPlaying.js
@@ -1,27 +1,34 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Hero from "../../components/Hero/Hero";
 import Movies from "../../components/Movies/Movies";
+import { useDispatch } from "react-redux";
+import ENDPOINTS from "../../utils/constants/endpoints";
+import { updateMovie } from "../../features/movieSlice";
 
 const NowPlaying = () => {
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const URL = `https://api.themoviedb.org/3/movie/now_playing/?api_key=${API_KEY}`;
+  // const API_KEY = process.env.REACT_APP_API_KEY;
+  // const URL = `https://api.themoviedb.org/3/movie/now_playing/?api_key=${API_KEY}`;
 
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getNowPLayingMovies();
   }, []);
 
   async function getNowPLayingMovies() {
-    const response = await axios(URL);
-    setMovies(response.data.results);
+    const response = await axios(ENDPOINTS.NOWPLAYING);
+    const movies = response.data.results;
+    // setMovies(response.data.results);
+    dispatch(updateMovie(movies));
   }
 
   return (
     <div>
       <Hero />
-      <Movies movies={movies} title="Now Playing Movies"/>
+      <Movies title="Now Playing Movies" />
     </div>
   );
 };
--- src/pages/movies/TopRated.js
+++ src/pages/movies/TopRated.js
@@ -1,27 +1,33 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Hero from "../../components/Hero/Hero";
 import Movies from "../../components/Movies/Movies";
+import { useDispatch } from "react-redux";
+import ENDPOINTS from "../../utils/constants/endpoints";
+import { updateMovie } from "../../features/movieSlice";
 
 const TopRated = () => {
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const URL = `https://api.themoviedb.org/3/movie/top_rated/?api_key=${API_KEY}`;
+  // const API_KEY = process.env.REACT_APP_API_KEY;
+  // const URL = `https://api.themoviedb.org/3/movie/top_rated/?api_key=${API_KEY}`;
 
-  const [movies, setMovies] = useState([]);
+  // const [movies, setMovies] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getNowTopRatedMovies();
   }, []);
 
   async function getNowTopRatedMovies() {
-    const response = await axios(URL);
-    setMovies(response.data.results);
+    const response = await axios(ENDPOINTS.TOPRATED);
+    // setMovies(response.data.results);
+    const movies = response.data.results;
+    dispatch(updateMovie(movies));
   }
 
   return (
     <div>
       <Hero />
-      <Movies movies={movies} title="Top Rated Movies"/>
+      <Movies title="Top Rated Movies" />
     </div>
   );
 };
